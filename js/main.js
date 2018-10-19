143071d65fd8731ccf5907e78ef35c3d42c48679
stevenjmarsh <sjm.atl@gmail.com>
2018-10-19 15:02:57 -0400

    adding notification event handlers

--- js/main.js
+++ js/main.js
@@ -23,13 +23,47 @@ const app = (() => {
   const notifyButton = document.querySelector('.js-notify-btn');
   const pushButton = document.querySelector('.js-push-btn');
 
-  // TODO 2.1 - check for notification support
+  if (!('Notification' in window)) {
+    console.log('This browser does not support notifications!');
+    return;
+  }
 
-  // TODO 2.2 - request permission to show notifications
+  Notification.requestPermission(status => {
+    console.log('Notification permission status:', status);
+  });
 
   function displayNotification() {
 
-    // TODO 2.3 - display a Notification
+    if (Notification.permission == 'granted') {
+      navigator.serviceWorker.getRegistration().then(reg => {
+
+        const options = {
+          body: 'First notification!',
+          icon: 'images/notification-flat.png',
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+          },
+
+          actions: [
+            {
+              action: 'explore', title: 'Go to the site',
+              icon: 'images/checkmark.png'
+            },
+            {
+              action: 'close', title: 'Close the notification',
+              icon: 'images/xmark.png'
+            },
+          ]
+
+          // TODO 5.1 - add a tag to the notification
+
+        };
+
+        reg.showNotification('Hello world!', options);
+      });
+    }
 
   }
 
